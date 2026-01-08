85f47c41859d692793f91c56c16bf81641864fcb
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-08 22:52:35 +0530

    A BB CCC DDDD special triangle pattern solution

---
+++ Main (7).java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int gap=3;
+	    int star=1;
+	    int a=65;
+		for(int i=1;i<=4;i++){
+		    
+		    for(int j=1;j<=gap;j++){
+		        System.out.print(" ");
+		        
+		    }
+		    for(int k=1;k<=star;k++){
+		        System.out.print((char)a);
+		        
+		    }
+		    gap--;
+		    star++;
+		    a++;
+		    System.out.println();
+		}
+	}
+}
