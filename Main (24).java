9c51dfd1f2fd557eac759d837a8a01a565f73a1b
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-11 19:07:34 +0530

    Find 1st element whose repeating in array

---
+++ Main (24).java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Arrays;
+public class Main
+{   
+    static int unique(int arr1[]){
+        for (int i=0;i<(arr1.length/2)+1;i++ ){
+            for(int j=i+1;j<arr1.length;j++){
+                if(arr1[i]==arr1[j]){
+                    return arr1[i];
+                }
+                
+            }
+        }
+        Arrays.sort(arr1);
+        
+        return -1;
+    }
+    
+	public static void main(String[] args) {
+		int x=12;
+		int arr[]={1,2,3,4,,2,3,1};
+		System.out.print(unique(arr.clone()));
+		
+	}
+}
