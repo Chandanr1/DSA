fc0a3a4d3a594b7640186c82601bc6b46376eef9
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-12 12:20:40 +0530

    Sort an array consisting of elements 0 & 1
    alternate approach of file main 28.java

---
+++ Main (29).java
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main
+{ 
+    static void sort(int arr[]){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) count++;
+        }
+        for(int i=0;i<count;i++){
+            arr[i]=0;
+        }
+        for(int i=count;i<arr.length;i++){
+            arr[i]=1;
+        }
+    }
+    static void swap(int arr[],int left,int right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+    }
+	public static void main(String[] args) {
+		int arr[]={0,0,1,0,0,1,1,0,1};
+		int left=0;
+		int right=arr.length-1;
+		while(left<right){
+		    if(arr[left]==1 && arr[right]==0) swap(arr,left,right);
+		    if(arr[left]==0) left++;
+		    if(arr[right]==1) right--;
+		}
+		for(int i=0;i<arr.length;i++){
+		    System.out.print(arr[i]+ " " );
+		}
+	}
+}
