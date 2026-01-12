5d383fc2f03a356955b5eac69423d1e866a58add
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-12 12:32:16 +0530

    sort s.t First even no. and then odd no. in array

---
+++ Main (30).java
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main
+{ 
+    static void sort(int arr[]){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) count++;
+        }
+        for(int i=0;i<count;i++){
+            arr[i]=0;
+        }
+        for(int i=count;i<arr.length;i++){
+            arr[i]=1;
+        }
+    }
+    static void swap(int arr[],int left,int right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+    }
+    static int[] order(int arr[]){
+        int arr1[]=new int[arr.length];
+        int a=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                arr1[a]=arr[i];
+                a++;
+            }
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                arr1[a]=arr[i];
+                a++;
+            }
+        }
+        return arr1;
+    }
+	public static void main(String[] args) {
+		int arr[]={1,2,3,5,7,4,6,8};
+		int arr2[]=order(arr);
+		for(int i=0;i<arr2.length;i++){
+		    System.out.print(arr2[i]+ " " );
+		}
+	}
+}
