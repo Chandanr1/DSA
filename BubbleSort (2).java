08bb94513e2510ecd72b9e4a0360e48ad22b04a5
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-18 11:55:35 +0530

    Bubble sort optimized(alternate approach)

---
+++ BubbleSort (2).java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class BubbleSort
+{
+    static void print(int arr[]){
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+    
+	public static void main(String[] args) {
+		int arr[]={1,3,2,-2,0,7,3};
+		print(arr);
+		for(int i=0;i<arr.length-1;i++){
+		    int swap=0;
+		    for(int j=0;j<arr.length-i-1;j++){
+		        if(arr[j]>arr[j+1]){
+		            int temp=arr[j];
+		            arr[j]=arr[j+1];
+		            arr[j+1]=temp;
+		            swap++;
+		        }
+		    }
+		    if(swap==0) break;
+		}
+		System.out.println();
+		print(arr);
+	}
+}
