b158eb34b1f0f26ae352c6decfef1ee1bcc8a657
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-01-28 18:39:25 +0530

    factorial of a number

---
+++ Main (53).java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{   
+    static void print(int a,int n){
+        if(a==n+1) return;
+        System.out.println(a);
+        print(a+1,n);
+    }
+    
+	public static void main(String[] args) {
+		int n=5;
+		int factorial=fact(n);
+		System.out.print(factorial);
+	}
+	static int fact(int n){
+	    if(n==1) return 1;
+	    return n*fact(n-1);
+	}
+	static void print1(int n){
+	    if(n==0) return;
+	    System.out.println(n);
+	    print1(n-1);
+	    System.out.println(n);
+	}
+}
