aa617d5ecd8b7072227cd000891b02a57c6de92a
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-02-18 18:24:57 +0530

    delete at tail in doubly linked list

---
+++ Main (63).java
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode prev;
+    ListNode(int val){
+        this.val=val;
+    }
+}
+class DLL{
+    ListNode head;
+    ListNode tail;
+    int size;
+    void insertAtHead(int val){
+        ListNode temp = new ListNode(val);
+        if(head==null) {
+            head=temp;
+            tail=temp;
+        }
+        else{
+            temp.next=head;
+            head.prev=temp;
+            head=temp;
+        }
+        size++;
+    }
+    void insertAtTail(int val){
+        ListNode temp = new ListNode(val);
+        if(head==null) {
+            head=temp;
+            tail=temp;
+        }
+        else{
+            tail.next=temp;
+            temp.prev=tail;
+            tail=temp;
+        }
+        size++;
+    }
+    void deleteAtHead(){
+       
+        if(head==null) {
+            System.out.print("list is empty");
+        }
+        else{
+            head=head.next;
+            head.prev=null;
+        }
+        size--;
+    }
+    void deleteAtTail(){
+       
+        if(head==null) {
+            System.out.print("list is empty");
+        }
+        else{
+            temp=temp.prev;
+            temp.next=null;
+        }
+        size--;
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
