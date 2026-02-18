9728bc38feee6aa7985f826aa657b169f908cc78
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-02-18 18:35:29 +0530

    Fix display method formatting in Main.java

--- Main (64).java
+++ Main (64).java
@@ -69,6 +69,7 @@ class DLL{
         ListNode temp=head;
         while(temp!=null){
             System.out.print(temp.val+" ");
+			temp=temp.next;
         }
         System.out.println();
     }
