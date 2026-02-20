4d069e519ae51f04b235b7f73ce6f14a2bdc7a98
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-02-20 16:13:10 +0530

    Stack implementation

---
+++ Main (68).java
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+//import java.util.Stack;
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+    }
+}
+class Stack{
+    Node head;
+    int len;
+    public void push(int val){
+        
+        Node temp=new Node(val);
+        if(len==0){
+            head=temp;
+        }
+        else{
+            temp.next=head;
+        head=temp;
+        }
+        len++;
+    }
+    public int pop(){
+        if(head==null){
+            System.out.print("Stack is empty");
+            return -1;
+        }
+        int b=head.val;
+        head=head.next;
+        len--;
+        return b;
+    }
+    int peek(){
+        if(head==null){
+            System.out.print("Stack is empty");
+            return -1;
+        }
+        return head.val;
+    }
+    void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+}
+public class Main
+{  
+	public static void main(String[] args) {
+	Stack st=new Stack();
+	st.push(10);
+	st.push(20);
+	st.push(30);
+	st.push(40);
+	st.display();
+	st.pop();
+	st.display();
+	}
+}
