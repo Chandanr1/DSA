0b1be1b6e3f0cee6620b57d4c6b8236f3e38e700
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-03-31 16:48:54 +0530

    maximum value in BST

---
+++ Main (73).java
@@ -0,0 +1,58 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class  Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+    }
+}
+class Main {
+    static int val=0;
+    public static void main(String[] args) {
+        Node a =new Node(3);
+		Node b=new Node(4);
+		Node c =new Node(2);
+		Node d =new Node(-1);
+		Node e =new Node(1);
+		Node f =new Node(6);
+		Node g =new Node(9);
+		a.left=b;a.right=c;
+		b.left=d;b.right=e;
+		c.left=f;c.right=g;
+		display(a);
+		System.out.print(max(a,0));
+	}
+	public static int max(Node root,int max){
+	    if(root==null) return max;
+	    if(root.val>max) max=root.val;
+	    int max1=max(root.left,max);
+	    int max2=max(root.right,max);
+	    return Math.max(root.val,Math.max(max1,max2));
+	}
+	public static int size(Node root){
+	    if(root==null) return 0;
+	    int lst=size(root.left);
+	    int rst=size(root.right);
+	    return 1+lst+rst;
+	}
+	public static int sum(Node root){
+	    if(root==null) return 0;
+	    
+	    return root.val+sum(root.left)+sum(root.right);
+	}
+	public static int multiply(Node root){
+	    if(root==null) return 1;
+	    
+	    return root.val*multiply(root.left)*multiply(root.right);
+	}
+	public static void display(Node root){
+	    if(root==null) return;
+	    //if(root==null) return ;
+	    val++;
+	    display(root.left);
+	    display(root.right);
+	    
+	}
+    }
