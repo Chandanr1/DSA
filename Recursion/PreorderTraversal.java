2b1d0e63d318639ab46eb4d72f366344e242701b
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-07-07 19:08:16 +0530

    Preorder Traversal (Iterative)

---
+++ Recursion/PreorderTraversal.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Solution {
+    
+    public ArrayList<Integer> preOrder(Node root) {
+        //  code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(st.size()>0){
+            Node top = st.pop();
+            ans.add(top.data);
+            if(top.right!=null) st.push(top.right);
+            if(top.left!=null) st.push(top.left);
+        }
+        return ans;
+    }
+}
