37709ce2a7079f4cb062f4f90618b6a48b9c3cdf
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2026-07-08 15:25:33 +0530

    Construct Tree from Inorder & Preorder
    Given two arrays representing the inorder and preorder traversals of a binary tree, your task is to construct the binary tree and return its root.
    
    Note: The inorder and preorder traversals contain unique values, and every value present in the preorder traversal is also found in the inorder traversal.

---
+++ Recursion/ConstructTree.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Solution {
+    public static Node build(int pl,int ph,int il,int ih , int in[],int pre[]){
+        if(pl>ph) return null;
+        Node root=new Node(pre[pl]);
+        int r=0;
+        for(int i=il;i<=ih;i++){
+            if(root.data==in[i]){
+                r=i;
+                break;
+            } 
+        }
+        int cnt=r-il;
+        root.left = build(pl+1,pl+cnt,il,ih-1,in,pre);
+        root.right = build(pl+cnt+1,ph,r+1,ih,in,pre);
+        return root;
+    }
+    public static Node buildTree(int inorder[], int preorder[]) {
+        // code here
+        int n = inorder.length;
+        return build(0,n-1,0,n-1,inorder,preorder);
+    }
+}
